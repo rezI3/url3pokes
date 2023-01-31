e225fd24486b8dfd9918e26e601d0704f8afc782
rez13 <cist.b220.y.katou@gmail.com>
2023-02-01 04:58:22 +0900

    うまくいかない部分をコメントアウト

--- frontend/pages/api/toPoke.ts
+++ frontend/pages/api/toPoke.ts
@@ -31,46 +31,47 @@ const handler = async (
   }
 
   // TODO: 文字列を数字3つに変換する処理
-  const pokeNumbers: PokeNumbers = toNumbers(url);
+  // const pokeNumbers: PokeNumbers = toNumbers(url);
   const pokes: Pokes = { first: "", second: "", third: "" };
 
   // TODO: APIを叩いて、図鑑番号からポケモン名に変換する処理
-  for (const key of Object.keys(pokeNumbers)) {
-    const pokeNumber = pokeNumbers[key];
-    const result = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokeNumber}`
-    );
+  for (const key of Object.keys(pokes)) {
+    // const pokeNumber: number = pokeNumbers[key];
+    // const result = await fetch(
+    //   `https://pokeapi.co/api/v2/pokemon/${pokeNumber}`
+    // );
 
-    const speciesUrl = result.data.species.url;
-    const responseSpecies = await fetch(speciesUrl);
-    const name = responseSpecies.data.names.find(
-      (v) => v.language.name === "ja"
-    );
+    // const speciesUrl = result.data.species.url;
+    // const responseSpecies = await fetch(speciesUrl);
+    // const name = responseSpecies.data.names.find(
+    //   (v) => v.language.name === "ja"
+    // );
 
-    pokes[key] = name;
+    // pokes[key] = name;
+    console.log(key)
   }
 
   res.status(200);
   return { pokes: pokes };
 };
 
-const toNumbers = (url: string): PokeNumbers => {
-  const urlArray = url.split("");
+// const toNumbers = (url: string): PokeNumbers => {
+//   const urlArray = url.split("");
 
-  const sum = urlArray
-    .map((char) => char.charCodeAt(0))
-    .reduce((acc, x) => acc + x);
+//   const sum = urlArray
+//     .map((char) => char.charCodeAt(0))
+//     .reduce((acc, x) => acc + x);
 
-  const numLen: number = Number(String(sum).length) / 3;
+//   const numLen: number = Number(String(sum).length) / 3;
 
-  const numArray = url.match(/.{numlen}/g);
-  const nums: PokeNumbers = {
-    first: (Number(numArray[0]) % 151) + 1,
-    second: (Number(numArray[1]) % 151) + 1,
-    third: (Number(numArray[2]) % 151) + 1,
-  };
+//   const numArray = url.match(/.{numlen}/g);
+//   const nums: PokeNumbers = {
+//     first: (Number(numArray[0]) % 151) + 1,
+//     second: (Number(numArray[1]) % 151) + 1,
+//     third: (Number(numArray[2]) % 151) + 1,
+//   };
 
-  return nums;
-};
+//   return nums;
+// };
 
 export default handler;
